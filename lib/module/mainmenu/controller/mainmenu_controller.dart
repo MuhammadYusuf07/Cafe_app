58055446fde18149143000085e3d42d6cbb8c7dd
MuhammadYusuf07 <yusuftkr077@gmail.com>
2023-11-07 10:09:49 +0700

    Sudah ganti semua menu Indo

--- lib/module/mainmenu/controller/mainmenu_controller.dart
+++ lib/module/mainmenu/controller/mainmenu_controller.dart
@@ -36,9 +36,9 @@ class MainmenuController extends State<MainmenuView> {
     {
       "id": 1,
       "photo":
-          "https://i.ibb.co/dG68KJM/photo-1513104890138-7c749659a591-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwy-ODA4-ODh8-MHwxf-H.jpg",
-      "product_name": "Frenzy Pizza",
-      "price": 25,
+          "https://asset.kompas.com/crops/VBs6hy5_8TPQwbLfAZRqZ3MwVN4=/556x0:5524x3312/750x500/data/photo/2022/07/18/62d4e8c69cef4.jpg",
+      "product_name": "Ayam Geprek",
+      "price": 15,
       "category": "Food",
       "description":
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
@@ -48,9 +48,9 @@ class MainmenuController extends State<MainmenuView> {
     {
       "id": 2,
       "photo":
-          "https://i.ibb.co/mHtmhmP/photo-1521305916504-4a1121188589-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwy-ODA4-ODh8-MHwxf-H.jpg",
-      "product_name": "Beef Burger",
-      "price": 22,
+          "https://asset.kompas.com/crops/yoovaRyPxaPFOY4gfCciore2eUY=/3x0:700x465/750x500/data/photo/2020/12/30/5fec5602f116e.jpg",
+      "product_name": "Ayam Bakar",
+      "price": 25,
       "category": "Food",
       "description":
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
@@ -60,8 +60,8 @@ class MainmenuController extends State<MainmenuView> {
     {
       "id": 3,
       "photo":
-          "https://images.unsplash.com/photo-1625869016774-3a92be2ae2cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      "product_name": "Seperait",
+          "https://asset.kompas.com/crops/9w-jxYs27CSSga6ZZngjxHur5t4=/0x0:4939x3293/780x390/data/photo/2022/09/25/63300cfd403f0.jpg",
+      "product_name": "Es Teh",
       "price": 33,
       "category": "Drink",
       "description":
@@ -72,9 +72,9 @@ class MainmenuController extends State<MainmenuView> {
     {
       "id": 4,
       "photo":
-          "https://images.unsplash.com/photo-1578160112054-954a67602b88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
-      "product_name": "Fried Rice",
-      "price": 31,
+          "https://img-global.cpcdn.com/recipes/71241e5eaaa9672f/1200x630cq70/photo.jpg",
+      "product_name": "Lele Bakar",
+      "price": 15,
       "category": "Food",
       "description":
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
@@ -84,9 +84,9 @@ class MainmenuController extends State<MainmenuView> {
     {
       "id": 5,
       "photo":
-          "https://i.ibb.co/mHCx9Nj/photo-1517487881594-2787fef5ebf7-crop-entropy-cs-tinysrgb-fit-max-fm-jpg-ixid-Mnwy-ODA4-ODh8-MHwxf-H.jpg",
-      "product_name": "Terrano Milk",
-      "price": 32,
+          "https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/akurat/gallery/2021/11/big/gal_618dd692a19465-28576895-85708281.JPG",
+      "product_name": "Jus Jeruk",
+      "price": 5,
       "category": "Drink",
       "description":
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
@@ -96,10 +96,10 @@ class MainmenuController extends State<MainmenuView> {
     {
       "id": 6,
       "photo":
-          "https://images.unsplash.com/photo-1626082927389-6cd097cdc6ec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      "product_name": "Fried Chicken",
-      "price": 49,
-      "category": "Food",
+          "https://asset.kompas.com/crops/UhV2ngrlUWo92yJyruxM7I-vSNE=/69x65:869x598/750x500/data/photo/2021/11/25/619f7dc86e939.jpg",
+      "product_name": "Bebek Goreng",
+      "price": 25,
+      "category": "20",
       "description":
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
       "discount_price": 45,
@@ -108,9 +108,9 @@ class MainmenuController extends State<MainmenuView> {
     {
       "id": 7,
       "photo":
-          "https://images.unsplash.com/photo-1525385133512-2f3bdd039054?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80",
-      "product_name": "Mango Juice",
-      "price": 62,
+          "https://asset.kompas.com/crops/3FlWFMVMqfZUNfEWap6lOvG8o10=/0x390:667x834/750x500/data/photo/2022/08/16/62fae87cb1142.jpeg",
+      "product_name": "Bebek Bakar",
+      "price": 25,
       "category": "Drink",
       "description":
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
@@ -120,9 +120,9 @@ class MainmenuController extends State<MainmenuView> {
     {
       "id": 8,
       "photo":
-          "https://images.unsplash.com/photo-1600271886742-f049cd451bba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      "product_name": "Orange Juice",
-      "price": 56,
+          "https://img-global.cpcdn.com/recipes/539561beae7b8af3/400x400cq70/photo.jpg",
+      "product_name": "Jus Mangga",
+      "price": 6,
       "category": "Drink",
       "description":
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
@@ -132,9 +132,9 @@ class MainmenuController extends State<MainmenuView> {
     {
       "id": 9,
       "photo":
-          "https://images.unsplash.com/photo-1604085792782-8d92f276d7d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
-      "product_name": "Avocado Juice",
-      "price": 56,
+          "https://kurio-img.kurioapps.com/21/05/12/49765d59-9376-4b35-b797-3a325f972c7b.jpe",
+      "product_name": "Jus Jambu",
+      "price": 5,
       "category": "Drink",
       "description":
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
@@ -144,9 +144,33 @@ class MainmenuController extends State<MainmenuView> {
     {
       "id": 10,
       "photo":
-          "https://images.unsplash.com/photo-1622240506921-042a4e71c172?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      "product_name": "Puncak Coffe",
-      "price": 56,
+          "https://images.tokopedia.net/img/cache/700/VqbcmM/2022/8/4/39bb8a03-913e-4ba6-91ca-05ccbdffb727.jpg",
+      "product_name": "Jus Alpukat ",
+      "price": 10,
+      "category": "Drink",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "discount_price": 50,
+      "is_favorite": true
+    },
+    {
+      "id": 11,
+      "photo":
+          "https://www.jagel.id/api/listimage/v/Jus-Apel--0-880618a217412916.jpg",
+      "product_name": "Jus Apel ",
+      "price": 10,
+      "category": "Drink",
+      "description":
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      "discount_price": 50,
+      "is_favorite": true
+    },
+    {
+      "id": 12,
+      "photo":
+          "https://cdn1-production-images-kly.akamaized.net/IKnE06y17S2jCDkXU0MyhAXGLVA=/1200x900/smart/filters:quality(75):strip_icc():format(webp)/kly-media-production/medias/2338494/original/090787200_1535012040-6.21_NAT_Pitaya_Wordpress-Body-Image.jpg",
+      "product_name": "Jus Buah Naga ",
+      "price": 8,
       "category": "Drink",
       "description":
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
